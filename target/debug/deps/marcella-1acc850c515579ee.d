c9a4c2b5a24b1237ae7ccd9769726e1cd706d175
Hugo Smith <huga@iinet.net.au>
2025-06-20 14:30:13 +0800

    run "cargo new marcella"

---
+++ target/debug/deps/marcella-1acc850c515579ee.d
@@ -0,0 +1,5 @@
+/home/beugo/marcella/marcella/target/debug/deps/libmarcella-1acc850c515579ee.rmeta: src/main.rs
+
+/home/beugo/marcella/marcella/target/debug/deps/marcella-1acc850c515579ee.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/marcella-d49e78c1d2d75e5a.d
@@ -0,0 +1,5 @@
+/home/beugo/marcella/marcella/target/debug/deps/libmarcella-d49e78c1d2d75e5a.rmeta: src/main.rs
+
+/home/beugo/marcella/marcella/target/debug/deps/marcella-d49e78c1d2d75e5a.d: src/main.rs
+
+src/main.rs:
